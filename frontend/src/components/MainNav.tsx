b803a719ee39cf7c49b5a84d17c8c8dbc8bc2289
Alan Teves Retubis <alanretubis@gmail.com>
2024-08-07 22:36:18 +0800

    :hammer: Added Router and Few Initial Layout Components.

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+    return(
+        <Button variant="ghost" className="font-bold hove:text-purple-500 hover:bg-white">
+            Log In
+        </Button>
+    );
+};
+
+export default MainNav;
