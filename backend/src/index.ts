8f0a602e4c3d7b2644e6a869bcce354a92acc355
Alan Teves Retubis <alanretubis@gmail.com>
2024-08-09 15:54:56 +0800

    :passport_control: Added Authorization Tokens.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
 import cors from "cors";
 // import "dotenv/config";
-require('dotenv').config()
-import mongoose from 'mongoose';
+require("dotenv").config();
+import mongoose from "mongoose";
 
-import myUserRoute from './routes/MyUserRoute';
+import myUserRoute from "./routes/MyUserRoute";
 
-mongoose.connect(`${process.env.MONGODB_CONNECTION_STRING}`).then(()=> console.log("Connected to database!"));
-
+mongoose
+  .connect(`${process.env.MONGODB_CONNECTION_STRING}`)
+  .then(() => console.log("Connected to database!"));
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use(cors());
 
 app.use("/api/my/user", myUserRoute);
 
-app.listen(8080, () => {
-    console.log("Server started.")
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+  console.log(`Server started on PORT: ${port}`);
 });
